ecc569a85b77c4a5d7adbf1b0ccf84802314f9c9
meenakshigurjar1010 <94699643+meenakshigurjar1010@users.noreply.github.com>
2021-11-19 21:18:00 +0530

    uploading initial file of calc

---
+++ calc.py
@@ -0,0 +1,31 @@
+n1=int(input("enter first number: "))
+n2=int(input("enter second number: "))
+
+print("Sum is-")
+print(n1+n2)
+
+print("Difference-")
+print(n1-n2)
+
+print("Divide-")
+print(n1/n2)
+
+print("Substract-")
+print(n1*n2)
+
+
+a=int(input("Enter any number: "))
+b=int(input("Enter any number: "))
+
+if b > a:
+    print("b is greater than a")
+
+elif a > b:
+    print("a is greter than b")
+
+else:
+    print("a is equal to b")
+
+
+
+
